d24df42a4cdd287fe1df4b30dff3ab6dd895a6cc
Sam Hayhurst <shayhurst@22cans.com>
2018-01-27 00:43:38 +0000

    * add week content to radio manager

--- Assets/Source/Radio/RadioManager.cs
+++ Assets/Source/Radio/RadioManager.cs
@@ -1,8 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public class RadioManager : MonoSingleton<RadioManager>
 {
 	[SerializeField] RadioWeek[] m_weeks;
+
+	public int Week { get; protected set; }
+	public int Day { get; protected set; }
+
+	// -------------------------------------------------------------------------------------------
+	void Start()
+	{
+		Week  = Day = 0;
+	}
+
+	// -------------------------------------------------------------------------------------------
+	void PlayDay(int _idx)
+	{
+
+	}
+
+	// -------------------------------------------------------------------------------------------
+	[CustomEditor(typeof(RadioManager))]	
+	public class RadioManagerEditor : Editor
+	{
+		RadioManager me;
+		public override void OnInspectorGUI ()
+		{
+			if (me == null)
+				me = RadioManager.Me;
+			if (me == null)
+				return;
+
+			if (GUILayout.Button ("Play Day 1"))
+			{
+				me.Week = me.Day = 0;
+				me.PlayDay (me.Day);
+			}
+
+			EditorGUILayout.LabelField ("Week", me.Week.ToString ());
+			EditorGUILayout.LabelField ("Day", me.Day.ToString ());
+
+			DrawDefaultInspector ();
+		}
+	}
 }
