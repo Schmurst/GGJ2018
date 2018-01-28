2e38a22d35ce54328d5261d330f0a530e8ae7297
Sam Hayhurst <shayhurst@22cans.com>
2018-01-28 03:25:34 +0000

    * prefab template for COde Input UI

---
+++ Assets/Source/CodeInput/CodeDigitController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CodeDigitController : MonoBehaviour
+{
+	static float m_zeroStart = 365f;
+	static float m_lowThreshold = 1000f;
+	static float m_lowTarget = 1000f;
+	static float m_highThreshold = 1000f;
+	static float m_highTarget = 1000f;
+
+	[SerializeField] Button m_buttonUp;
+	[SerializeField] Button m_buttonDown;
+
+
+
+	void Start ()
+	{
+		
+	}
+
+	void Update ()
+	{
+		
+	}
+}
