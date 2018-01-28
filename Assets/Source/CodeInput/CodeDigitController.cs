b88093679db2fa33660d85e9699633c7a6df499f
Sam Hayhurst <shayhurst@22cans.com>
2018-01-28 12:20:05 +0000

    * working version of code selector

--- Assets/Source/CodeInput/CodeDigitController.cs
+++ Assets/Source/CodeInput/CodeDigitController.cs
@@ -1,28 +1,54 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class CodeDigitController : MonoBehaviour
 {
-	static float m_zeroStart = 365f;
-	static float m_lowThreshold = 1000f;
-	static float m_lowTarget = 1000f;
-	static float m_highThreshold = 1000f;
-	static float m_highTarget = 1000f;
-
 	[SerializeField] Button m_buttonUp;
 	[SerializeField] Button m_buttonDown;
+	[SerializeField] RectTransform m_scrollTrans;
 
+	public int Value;
+	float m_targetPosition;
 
-
-	void Start ()
+	public void Initialise()
 	{
-		
+		ResetToZero ();
+		m_buttonUp.onClick.AddListener (ValueUp);
+		m_buttonDown.onClick.AddListener (ValueDown);
+	}
+
+	void ValueUp()
+	{
+		Value++;
+		m_targetPosition = m_targetPosition - CodeInputController.Me.DigitHeight;
+	}
+
+	void ValueDown()
+	{
+		Value--;
+		m_targetPosition = m_targetPosition + CodeInputController.Me.DigitHeight;
+	}
+
+	public void ResetToZero()
+	{
+		m_scrollTrans.anchoredPosition = new Vector2(0f, CodeInputController.Me.DigitZeroPos);
+		m_targetPosition = m_scrollTrans.localPosition.y;
+		Value = 0;
 	}
 
 	void Update ()
 	{
-		
+		var pos = m_scrollTrans.anchoredPosition;
+		if (pos.y > CodeInputController.Me.DigitUpperLimit)
+			m_targetPosition = pos.y = CodeInputController.Me.DigitUpperReset;
+		if (pos.y < CodeInputController.Me.DigitLowerLimit)
+			m_targetPosition = pos.y = CodeInputController.Me.DigitLowerReset;
+
+		pos = Vector2.Lerp (pos, Vector2.up * m_targetPosition, CodeInputController.Me.DigitLerpPower);
+
+		m_scrollTrans.anchoredPosition = pos;
 	}
 }
--- Assets/Source/CodeInput/CodeInputController.cs
+++ Assets/Source/CodeInput/CodeInputController.cs
@@ -2,15 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CodeInputController : MonoBehaviour {
+public class CodeInputController : MonoSingleton<CodeInputController>
+{
+	[SerializeField] CodeDigitController[] m_digits; 
 
-	// Use this for initialization
-	void Start () {
-		
+	public float DigitZeroPos = 365f;
+	public float DigitRotorHeight = 800f;
+	public float DigitHeight = 80f;
+	public float DigitUpperLimit;
+	public float DigitUpperReset;
+	public float DigitLowerLimit; 
+	public float DigitLowerReset;
+	public float DigitLerpPower = 0.1f;
+
+	void Start()
+	{
+		for (int i = 0; i < m_digits.Length; i++)
+			m_digits [i].Initialise ();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	void ResetAllDigits()
+	{
+		for (int i = 0; i < m_digits.Length; i++)
+			m_digits [i].ResetToZero ();
 	}
 }
