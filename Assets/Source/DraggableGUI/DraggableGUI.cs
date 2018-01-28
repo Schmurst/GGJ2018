389fb018643af005b195e94426c39941b64238fe
Sam Hayhurst <shayhurst@22cans.com>
2018-01-28 01:25:42 +0000

    * really dirty drag and drop system

--- Assets/Source/DraggableGUI/DraggableGUI.cs
+++ Assets/Source/DraggableGUI/DraggableGUI.cs
@@ -1,16 +1,61 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
 
-public class DraggableGUI : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
+public class DraggableGUI : MonoBehaviour
+{
+	enum InputState
+	{
+		nothing,
+		hover,
+		grabbed
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	static float s_grabScale = 1.05f;
+	static Color s_hoverTint = new Color(0.9f, 0.9f, 0.9f);
+
+	[SerializeField] Image m_img;
+
+	InputState m_state;
+	Vector2 m_grabPosOffset;
+
+	void Start()
+	{
+		DraggableGUIManager.Me.RegisterDraggable (this);
+	}
+
+	public void OnPointerEnter()	{m_state = InputState.hover;}
+	public void DeSelect()			{m_state = InputState.nothing;}
+	public void OnPointerExit()		{m_state = InputState.nothing;}
+	public void OnPointerUp()		{m_state = InputState.nothing;}
+	public void OnPointerDown()	
+	{
+		m_state = InputState.grabbed;
+		Debug.LogFormat ("MousePos: {0}", Input.mousePosition);
+		m_grabPosOffset = this.transform.position - Input.mousePosition;
+	}
+
+	void Update()
+	{
+		switch (m_state)
+		{
+		default:
+		case InputState.nothing:
+			m_img.color = Color.white;
+			transform.localScale = Vector3.one;
+			return;
+		case InputState.hover:
+			m_img.color = s_hoverTint;
+			transform.localScale = Vector3.one;
+			break;
+		case InputState.grabbed:
+			m_img.color = s_hoverTint;
+			transform.position = m_grabPosOffset + (Vector2)Input.mousePosition;
+			transform.localScale = Vector3.one * s_grabScale;
+			break;
+		}
 	}
 }
--- Assets/Source/DraggableGUI/DraggableGUIManager.cs
+++ Assets/Source/DraggableGUI/DraggableGUIManager.cs
@@ -4,5 +4,36 @@ using UnityEngine;
 
 public class DraggableGUIManager : MonoSingleton<DraggableGUIManager>
 {
-	
+	List<DraggableGUI> m_draggables;
+	DraggableGUI m_current;
+
+	[SerializeField] GameObject m_codeBook1;
+	[SerializeField] GameObject m_codeBook2;
+	[SerializeField] GameObject m_codeBook3;
+
+	public override void Init ()
+	{
+		m_draggables = new List<DraggableGUI> ();
+	}
+
+	public void RegisterDraggable(DraggableGUI _instance)
+	{
+		m_draggables.Add (_instance);
+	}
+
+	public void Select(DraggableGUI _selected)
+	{
+		if (m_current != null)
+			m_current.DeSelect ();
+		m_current = _selected;
+	}
+
+	void Update()
+	{
+		if (Input.GetMouseButtonUp(0) && m_current != null)
+		{
+			m_current.DeSelect ();
+			m_current = null;
+		}
+	}
 }
